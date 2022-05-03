b2e60388a9ae68feb90861b5f569309c14007156
sanjita2204 <104855438+sanjita2204@users.noreply.github.com>
2022-05-03 22:51:09 +0530

    Add files via upload

---
+++ assign1 data structures.py
@@ -0,0 +1,14 @@
+#assignment 1 - data structures
+# 1. area of circle
+
+r = float(1.1)
+area = 3.14*r*r
+print("Area of the circle is : ",area )
+
+
+#2.extension of file
+
+filename = input("Enter the filename: ")
+f_extns = filename.split(".")
+print("Extension of the file is : ",f_extns[-1])
+
