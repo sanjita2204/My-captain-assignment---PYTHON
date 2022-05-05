5a895e45c60468de8174bc001f4383bb9e1251cf
sanjita2204 <104855438+sanjita2204@users.noreply.github.com>
2022-05-05 20:23:03 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,12 @@
+list_1 = [12,-7,5,64,-14]
+for x in list_1:
+    if x < 0 :
+        list_1.remove(x)
+print(list_1)
+
+    
+list_2 = [12,14,-95,3]
+for x in list_2 :
+    if x < 0 :
+        list_2.remove(x)
+print(list_2)
